c8ef9ecc3d8768c745d95a77ec27cd7a7ca10b8f
MrPPak <matht1115@naver.com>
2022-01-11 16:26:54 +0900

    feat: 게시물 등록 및 조회 view단 완성

--- 스프링 부트와 AWS로 혼자 구현하는 웹서비스/spring_project01/src/main/java/com/mrppak/spring_project01/springboot/service/posts/PostsService.java
+++ 스프링 부트와 AWS로 혼자 구현하는 웹서비스/spring_project01/src/main/java/com/mrppak/spring_project01/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.mrppak.spring_project01.springboot.service.posts;
 
 import com.mrppak.spring_project01.springboot.domain.posts.Posts;
 import com.mrppak.spring_project01.springboot.domain.posts.PostsRepository;
+import com.mrppak.spring_project01.springboot.web.dto.PostsListResponseDto;
 import com.mrppak.spring_project01.springboot.web.dto.PostsResponseDto;
 import com.mrppak.spring_project01.springboot.web.dto.PostsSaveRequestDto;
 import com.mrppak.spring_project01.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional
@@ -31,6 +35,13 @@ public class PostsService {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시물이 없습니다. id = " + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
