c742dcd6c7ac6c98ef100c46ee4cc6b32c3dd8b4
MrPPak <matht1115@naver.com>
2022-01-11 09:43:03 +0900

    feat: HelloContrller 완성 및 테스트 완료

---
+++ 스프링 부트와 AWS로 혼자 구현하는 웹서비스/spring_project01/src/test/java/com/mrppak/spring_project01/springboot/web/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.mrppak.spring_project01.springboot.web;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void helloDtoTest() throws Exception {
+
+        String name = "학생";
+        int amount = 5;
+
+        mvc.perform(
+                MockMvcRequestBuilders.get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name", Matchers.is(name)))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.amount", Matchers.is(amount)));
+    }
+}
